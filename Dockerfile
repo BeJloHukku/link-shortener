e5b3c10bb2f59c0e23c67a4c3e668a5dc780a083
BeJloHUkku <igoryashamb@yandex.ru>
2025-12-06 21:23:40 +0300

    Create Dockerfile for app and add app to docker-compose.yaml

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.13-slim AS builder
+
+WORKDIR /app
+
+RUN pip install uv
+
+COPY pyproject.toml uv.lock ./
+
+RUN uv sync --frozen
+
+FROM python:3.13-slim
+
+WORKDIR /app
+
+COPY --from=builder /app/.venv /app/.venv
+
+COPY . . 
+
+EXPOSE 8000
+
+ENV PATH="/app/.venv/bin:$PATH"
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- datebase/db.py
+++ datebase/db.py
@@ -1,4 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "6532")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(
     url="postgresql+asyncpg://postgres:postgres@localhost:6532/postgres"
