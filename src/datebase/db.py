37fdef58e8f1ff53681b5991585cab8eb828237d
BeJloHukku <igoryashamb@gmail.com>
2025-12-10 17:43:21 +0300

    Pushed docker images to Docker Hub and changed docker-compose.uml

--- src/datebase/db.py
+++ src/datebase/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "6532")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_NAME = os.getenv("DB_NAME", "postgres")
